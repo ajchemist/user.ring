77756f96d3fd55d365490f4bed54f0a49177c073
ajchemist <1694505+aJchemist@users.noreply.github.com>
2020-12-15 23:39:33 +0900

    CI transition: travis -> gh-actions

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ((clojure-mode
-  (cider-clojure-cli-global-options . "-A:provided:test")
+  ;; (cider-clojure-cli-global-options . "-M:provided:test")
+  (cider-clojure-cli-parameters . "-M:provided:test -m nrepl.cmdline --middleware '%s'")
   (clojure-local-source-path . "src/core")
   (clojure-local-source-test-path . "src/test")))
