8c3a93380a2a29813d91f7ed274a964aedaad568
magneto <magneto@magneto-note.local>
2022-02-08 10:56:05 +0900

    pre-init-spec ::ig.user.ring/jetty-server

--- src/core/user/ring/alpha/model/integrant.clj
+++ src/core/user/ring/alpha/model/integrant.clj
@@ -1,11 +1,19 @@
 (ns user.ring.alpha.model.integrant
   (:require
+   [clojure.spec.alpha :as s]
    [integrant.core :as ig]
    [ring.adapter.jetty :as jetty]
    ))
 
 
 (set! *warn-on-reflection* true)
+
+
+(s/def :ring/handler fn?)
+
+
+(defmethod ig/pre-init-spec ::jetty-server [_]
+  (s/keys :req-un [:ring/handler]))
 
 
 (defmethod ig/init-key ::jetty-server
